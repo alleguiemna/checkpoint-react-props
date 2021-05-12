d7116a490df44ef2e6f2709ea2353eb4aa9a450a
alleguiemna <allegui.emna@yahoo.fr>
2021-05-12 12:55:56 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Profile from "./profile/Profile/Profile";
+import Header from "./profile/Profile/header/Header";
+import Footer from "./profile/Profile/footer/Footer";
+import Bio from "./profile/Profile/Bio";
+
 
 function App() {
+  const fullName = "Allegui Emna";
+  const bio = <Bio />;
+  const profession = "Etudiante chez Go My Code";
+  const handleName = (name) => {
+    alert(`The name of the profile user is ${name}`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <div>
+      <img
+        src="https://celebrity.tn/wp-content/uploads/2019/02/noura-samet-1024x1024.jpg"
+        alt="description"
+        height="200"
+        width="200"
+        style={{ borderRadius: "50%",marginTop:"30px",marginLeft:"150px" }}
+      ></img>
+      
+      <Profile
+        fullName={fullName}
+        bio={bio}
+        profession={profession}
+        handleName={handleName}
+      />
+      </div>
+      
+      <Footer />
     </div>
   );
 }
---
+++ src/profile/Profile/footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Home, Search, ShoppingBag, Video } from "react-feather";
+
+const Footer = () => {
+  return (
+    <div calssName="">
+      <Navbar bg="light" className="d-flex justify-content-around">
+        <Home /> <Search /> <Video /> <ShoppingBag />{" "}
+        <img
+          className="p-2 col-example text-left"
+          src="https://celebrity.tn/wp-content/uploads/2019/02/noura-samet-1024x1024.jpg"
+          alt="description"
+          height="60px"
+          width="60px"
+          style={{ borderRadius: "50%" }}
+        />
+      </Navbar>
+      
+    </div>
+  );
+};
+
+export default Footer;
